6b35f9c097d493935acfd4adb1660864742cfc16
Koalaven <102919896+Koalaven@users.noreply.github.com>
2025-12-04 21:11:45 -0500

    slim down

--- Controller Code/Old Controller Codes/Controller code esp-idf/esp32s3_hid_gamepad_buttons/main/button_config.c
+++
@@ -1,8 +0,0 @@
-#include "button_config.h"
-
-button_config_t button_map[NUM_BUTTONS] = {
-    { "A",  GPIO_NUM_1,  (1 << 0) },
-    { "B",  GPIO_NUM_2,  (1 << 1) },
-    { "X",  GPIO_NUM_3,  (1 << 2) },
-    { "Y",  GPIO_NUM_4,  (1 << 3) },
-};
